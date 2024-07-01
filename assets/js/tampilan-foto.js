9ca24c8c40e1ffcdb6069e16a8e874d0f9f6ea7e
adnemicano <anandaliladmy@gmail.com>
2024-07-01 11:54:10 +0700

    first commit

---
+++ assets/js/tampilan-foto.js
@@ -0,0 +1,9 @@
+function showForm() {
+    document.getElementById("form-container").style.display = "block";
+    document.getElementById("form-container2").style.display = "none"; // hide the other form
+}
+
+function showForm2() {
+    document.getElementById("form-container2").style.display = "block";
+    document.getElementById("form-container").style.display = "none"; // hide the other form
+}
